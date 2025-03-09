01ec234c04da0894a775cf3ef0607abbc379521d
cmb347827 <v38d422ll621dem@gmail.com>
2025-03-09 09:51:36 -0400

    navigation menu changes

--- README-template.md
+++ README-template.md
@@ -51,10 +51,12 @@ Users should be able to:
 ### What I learned
 
 - I had some trouble keeping the buttons for the carousel in place , as they appear above the image/text in the carousel html, and 
-  they had to appear steady in different positions for mobile and desktop. 
+  they had to appear steady in different positions for mobile and desktop. I first tried to solve this with `transform:translate(50%,50%);` , different values for the x and y movements, but this didn't work as hoped. When I resized the screen, it worked to stay in place , for a bit, but not the whole range of resizing. 
   I found a stackoverflow post that explained how to get the position of an element , relative to another, that's not it's parent (link 1). Then I found another stackoverflow post that explained the use off offsetHeight (link 2) and I figured it out for the rest from there (see my offsetCalculate() function).
 - Next, I had a hard time to make sure when resizing the screen, that the top carousel section and bottom aside section kept the same width, and that the text in the aside section didn't spill out beneath the images height. I tried a combination of using '.  img-fluid' , '.flex-noshrink-md-1', and '.flex-basis-40-md' . In the end  just using `.img-fluid` for the aside images, and `.flex-noshrink-md-1` for both the carousel text and aside text , in combination with setting width for the carousel text, worked.
 - Next, was the images that were the same but different sizes. I know that the `<picture>` element works for this, to force a change at different screensizes. Or `<img>` with `scrcet` can be used, but then it's up to the browswer to determine when to change the image. 
+- I had to figure out how to resize the left/right arrow svg. First I tried this by simply adding padding to the svg, but this did not work , as the svg dissapeared. I tried by adding `transform: scale(3);` to the svg, and by adding a larger width/height value than the svg width/height to the parent button element, and this worked.
+- I used the W3 carousel pattern.
 
     
  
--- room-homepage-master.js
+++ room-homepage-master.js
@@ -8,8 +8,12 @@ $(window).resize(function(){
 const data ={
 	 navBtn: document.getElementById('menubutton'),
 	 navBar: document.getElementById('navbarCollapse'),
+	 nav: document.getElementById('nav'),
 	 currentIndex: 0,
 	 carouselItems : document.querySelectorAll('.carousel-item'),
+	 header: document.getElementById('header'),
+	 close: document.getElementById('close'),
+	 open: document.getElementById('open'),
 }
 
 
@@ -26,13 +30,13 @@ function offsetCalculate(index){
 		if(index===picindex){
 			if(window.innerWidth > 768){
 				$('.controls').css({
-					'top':pic.offsetHeight - 34,
+					'top':pic.offsetHeight -85,
 					'left': pic.offsetWidth,
 				});
 			}else if(window.innerWidth <= 767){
 				$('.controls').css({
-					'top':pic.querySelector(':nth-child(2)').offsetHeight,
-					'left': pic.querySelector(':nth-child(2)').offsetWidth - (34*2),
+					'top':pic.querySelector(':nth-child(2)').offsetHeight -85,
+					'left': pic.querySelector(':nth-child(2)').offsetWidth - (70* 2),
 				});
 			}
 		}
@@ -66,10 +70,39 @@ $(window).on('load',function(){
     offsetCalculate(0);
 	addListener();
 	data.navBtn.addEventListener('click', function(){
-		$(data.navBar).toggleClass('is-active');
-		$('#open').toggleClass('hidden');
-		$('#close').toggleClass('hidden');
+		
+		/*if(!(data.navBar.classList.contains('nav-class')) && !(data.navBar.classList.contains('collapse-menu'))){
+			data.navBar.classList.add('nav-class','collapse-menu');
+		}else{
+			data.navBar.classList.remove('nav-class','collapse-menu');
+		}*/
+		if(!(data.navBar.classList.contains('collapse-menu'))){
+			data.navBar.classList.add('collapse-menu');
+		}else{
+			data.navBar.classList.remove('collapse-menu');
+		}
+		if(!(data.nav.classList.contains('nav-class'))){
+			data.nav.classList.add('nav-class');
+		}else{
+			data.nav.classList.remove('nav-class');
+		}
+		if(!(data.open.classList.contains('hidden'))){
+			data.open.classList.add('hidden');
+		}else{
+			data.open.classList.remove('hidden');
+		}
+		if(!(data.close.classList.contains('hidden'))){
+			data.close.classList.add('hidden');
+		}else{
+			data.close.classList.remove('hidden');
+		}
+	
+		//$('#open').toggleClass('hidden');
+		//$('#close').toggleClass('hidden');
+		data.close.toggleAttribute('aria-hidden');
 	    data.navBtn.toggleAttribute('aria-expanded');
+       
+		
 	});
 });
 
